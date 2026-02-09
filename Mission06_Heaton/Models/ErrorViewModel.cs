ab7ef2a872b9d3981462d343b67a4ec80b95b37f
Aceluke24 <aceluke24@gmail.com>
2026-02-09 13:19:08 -0700

    Initial commit

---
+++ Mission06_Heaton/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Mission06_Heaton.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
